a715bc923ea33dd6b675e2b0ab40c7382508c036
Ali Fakhry <sfakhry@umw.edu>
2024-03-21 08:04:35 -0400

    updated farmerv3.zork with exits.

--- src/Command.java
+++ src/Command.java
@@ -1,55 +1,65 @@
-class Command{
+abstract class Command{
 
-    private String dir;
+    abstract String execute();
+    
 
 
-    Command(String dir){
-        this.dir = dir;
-    }
+}   
 
-    String execute(){
+
+
+
+
+ //   private String dir;
+
+
+//    Command(String dir){
+//        this.dir = dir;
+//    }
+
+//    String execute(){
         //Room room = GameState.instance().visit(); 
 
-    if (dir.equals("save")) {
-            try {
-                // Step 3: Call GameState::store() when "save" is entered
-                //String saveName = "save.zork";
-                GameState.instance().store("files/save.sav");
-                System.out.println("Game saved successfully.");
-            } catch (Exception e) {
-                // Handle file not found exception
-                System.err.println("Error: Could not save the game.");
-                e.printStackTrace();
-            }
-    }
+//    if (dir.equals("save")) {
+//            try {
+//                // Step 3: Call GameState::store() when "save" is entered
+//                //String saveName = "save.zork";
+//                GameState.instance().store("files/save.sav");
+//                System.out.println("Game saved successfully.");
+//            } catch (Exception e) {
+//                // Handle file not found exception
+//                System.err.println("Error: Could not save the game.");
+//                e.printStackTrace();
+//            }
+//    }
 
             
     
-    else if(dir.equals("restore")){
-            try {
-                GameState.instance().restore("files/save.sav");
-                System.out.println("Game restored successfully.");
-            } catch (Exception e) {
-                System.err.println("Error: Could not restore the game.");
-                e.printStackTrace();
-            }
+//    else if(dir.equals("restore")){
+//            try {
+//                GameState.instance().restore("files/save.sav");
+//                System.out.println("Game restored successfully.");
+//            } catch (Exception e) {
+//                System.err.println("Error: Could not restore the game.");
+//                e.printStackTrace();
+//            }
+//
+//        }
 
-        }
 
-
-        Room currentRoom  = GameState.instance().getAdventurersCurrentRoom();
+//        Room currentRoom  = GameState.instance().getAdventurersCurrentRoom();
      //   System.out.println("works- currentRoom");
-        Room nextRoom = currentRoom.leaveby(dir);
+//        Room nextRoom = currentRoom.leaveby(dir);
         
-        if(nextRoom != null){
+//        if(nextRoom != null){
      //   System.out.println("works!!!");
-        }
+//        }
      //   else{
      //   System.out.println("No Exit in this direction");
      //   }
 
-        return nextRoom.describe(); 
+//        return nextRoom.describe(); 
 
-    }
-}
+//    }
+//}
 
--- src/CommandFactory.java
+++ src/CommandFactory.java
@@ -14,7 +14,29 @@ public class CommandFactory {
     }
 
     Command parse(String commandString) {
-        return new Command(commandString);
+ //       return new Command(commandString);
         //return new MovementCommand(commandString);
+   
+   String[] parseParts = commandString.split(" ", 2);
+   String commandTypes = parseParts[0].toLowerCase();
+
+   if(commandTypes.equals("take")){   
+       String item;          //assigns the second part of the command to 
+       if(parseParts.length > 1){
+       item = parseParts[1];
+       }else{
+           item = "";
+       }
+   
+   return new TakeCommand(item);
+     }
+   
+
+    else{
+    return new UnknownCommand(commandString);
     }
+    
+    }
+
 }
+
